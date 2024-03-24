329635ee6dafdd3d748a4121acb70bd2abd5ecb0
s27051 <s27051@pjwstk.edu.pl>
2024-03-24 19:41:43 +0100

    Added base values

--- Program/PSM3/main.py
+++ Program/PSM3/main.py
@@ -1,4 +1,8 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+
+# Base Values
+g = 9.81  # Acceleration due to gravity (m/s^2)
+L = 1     # Length of the pendulum (m)
+m = 1     # Mass of the pendulum (kg)
