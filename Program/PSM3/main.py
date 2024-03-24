0b23c64796cd35cd81e04524fa5b107094f8e172
s27051 <s27051@pjwstk.edu.pl>
2024-03-24 20:09:49 +0100

    Fixed bug with wrong indent

--- Program/PSM3/main.py
+++ Program/PSM3/main.py
@@ -115,26 +115,26 @@ def animate_pendulum(theta, title):
     ani = animation.FuncAnimation(fig, animate, init_func=init, frames=len(theta), interval=20, blit=True)
     plt.show()
 
-    # Perform simulations
-    theta_euler, omega_euler, t_euler = simulate(euler_step)
-    theta_midpoint, omega_midpoint, t_midpoint = simulate(midpoint_step)
-    theta_rk4, omega_rk4, t_rk4 = simulate(rk4_step)
-    theta_rk5, omega_rk5, t_rk5 = simulate(rk5_step)
+# Perform simulations
+theta_euler, omega_euler, t_euler = simulate(euler_step)
+theta_midpoint, omega_midpoint, t_midpoint = simulate(midpoint_step)
+theta_rk4, omega_rk4, t_rk4 = simulate(rk4_step)
+theta_rk5, omega_rk5, t_rk5 = simulate(rk5_step)
 
-    # Calculate energies
-    pe_euler, ke_euler, te_euler = calculate_energy(theta_euler, omega_euler)
-    pe_midpoint, ke_midpoint, te_midpoint = calculate_energy(theta_midpoint, omega_midpoint)
-    pe_rk4, ke_rk4, te_rk4 = calculate_energy(theta_rk4, omega_rk4)
-    pe_rk5, ke_rk5, te_rk5 = calculate_energy(theta_rk5, omega_rk5)
+# Calculate energies
+pe_euler, ke_euler, te_euler = calculate_energy(theta_euler, omega_euler)
+pe_midpoint, ke_midpoint, te_midpoint = calculate_energy(theta_midpoint, omega_midpoint)
+pe_rk4, ke_rk4, te_rk4 = calculate_energy(theta_rk4, omega_rk4)
+pe_rk5, ke_rk5, te_rk5 = calculate_energy(theta_rk5, omega_rk5)
 
-    plot_energy(t_euler, pe_euler, ke_euler, te_euler, 'Euler')
-    animate_pendulum(theta_euler, 'Pendulum Motion Animation - Euler Method')
+plot_energy(t_euler, pe_euler, ke_euler, te_euler, 'Euler')
+animate_pendulum(theta_euler, 'Pendulum Motion Animation - Euler Method')
 
-    plot_energy(t_midpoint, pe_midpoint, ke_midpoint, te_midpoint, 'Midpoint')
-    animate_pendulum(theta_midpoint, 'Pendulum Motion Animation - Midpoint Method')
+plot_energy(t_midpoint, pe_midpoint, ke_midpoint, te_midpoint, 'Midpoint')
+animate_pendulum(theta_midpoint, 'Pendulum Motion Animation - Midpoint Method')
 
-    plot_energy(t_rk4, pe_rk4, ke_rk4, te_rk4, 'RK4')
-    animate_pendulum(theta_rk4, 'Pendulum Motion Animation - RK4 Method')
+plot_energy(t_rk4, pe_rk4, ke_rk4, te_rk4, 'RK4')
+animate_pendulum(theta_rk4, 'Pendulum Motion Animation - RK4 Method')
 
-    plot_energy(t_rk5, pe_rk5, ke_rk5, te_rk5, 'RK5')
-    animate_pendulum(theta_rk5, 'Pendulum Motion Animation - RK5 Method')
+plot_energy(t_rk5, pe_rk5, ke_rk5, te_rk5, 'RK5')
+animate_pendulum(theta_rk5, 'Pendulum Motion Animation - RK5 Method')
