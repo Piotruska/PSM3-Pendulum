41849709cd9ec8ef44e2d26f54a09ff096e53a48
s27051 <s27051@pjwstk.edu.pl>
2024-03-24 19:44:36 +0100

    Added Initial conditions

--- Program/PSM3/main.py
+++ Program/PSM3/main.py
@@ -6,3 +6,11 @@ import matplotlib.animation as animation
 g = 9.81  # Acceleration due to gravity (m/s^2)
 L = 1     # Length of the pendulum (m)
 m = 1     # Mass of the pendulum (kg)
+
+# Initial conditions
+theta0 = np.pi / 3  # Initial angle (radians)
+omega0 = 0          # Initial angular velocity (radians/second)
+t0 = 0              # Initial time (s)
+tmax = 100         # Maximum time (s)
+dt = 0.01           # Time step (s)
+
